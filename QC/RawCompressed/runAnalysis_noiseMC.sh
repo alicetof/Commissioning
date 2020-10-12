acdb9dbbe7ee45e28c893073995d6cffca7391be
Francesca Ercolessi <francesca.ercolessi@cern.ch>
2020-10-12 09:49:49 +0200

    renamed analysis script deleted already existing macro

--- QC/RawCompressed/runAnalysis_noiseMC.sh
+++ QC/RawCompressed/runAnalysis_noiseMC.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
 
-#VERBOSE="--tof-compressor-verbose --tof-compressor-decoder-verbose --tof-compressor-encoder-verbose --tof-compressor-checker-verbose"
-VERBOSE=""
-
 if [ -z "$1" ]; then
-    echo "usage: ./noise_MCanalysis [fileout]"
+    echo "usage: ./runAnalysis_noiseMC.sh [fileout]"
     exit 1
 fi
 FILEOUT=$1
@@ -14,6 +11,6 @@ o2-raw-file-reader-workflow -b --input-conf o2-raw-file-reader-workflow.ini \
     o2-tof-compressor -b \
     | \
     o2-tof-compressed-analysis -b \
-			       --tof-compressed-analysis-filename noise_analysis.C \
+			       --tof-compressed-analysis-filename ../../ANALYSIS/noise/noise_analysis.C \
 			       --tof-compressed-analysis-function "noise_analysis(4096, 36864, \"$FILEOUT\")" \
 			       --run

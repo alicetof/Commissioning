22d1bebf17238b00f11df989e6e04912f321ea8c
noferini <9963644+noferini@users.noreply.github.com>
2024-06-25 12:03:37 +0200

    bug fix

--- QC/automaticProcess/install.sh
+++ QC/automaticProcess/install.sh
@@ -4,12 +4,16 @@ source config
 mkdir -p $MAIN_PATH
 cp * $MAIN_PATH/.
 
-mkdir -p $PATH_QC
+mkdir -p $PATH_QC/2024
 mkdir -p $PATH_CALIB
 mkdir -p $PATH_CERNBOX
 
+mkdir template/eos
+mkdir templateMC/eos
 cp -r template $PATH_QC/.
 cp -r templateMC $PATH_QC/.
+cp -r template $PATH_QC/2024/cpass0
+cp -r template $PATH_QC/2024/apass1
 cp calib/* $PATH_CALIB/.
 cp QC/* $PATH_QC/.
 
